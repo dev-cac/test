4d7db6eca01a03d53b755c1ab25487f7a14f069f
luisfosg <luisfosgdev@gmail.com>
2023-09-18 22:05:47 -0500

    feat: creando pdfs con html-pdf

---
+++ pdf.js
@@ -0,0 +1,27 @@
+const pdf = require("html-pdf");
+const fs = require("fs");
+
+const template = fs.readFileSync(
+    __dirname + "/product.html"
+).toString("utf8");
+
+const header = `
+    <h4 style="text-align: end">Fecha:${'Hola Mundo :D'}</h4>
+    <h2>Lista de productos </h2>
+`
+
+const options = {
+    header: { contents: header },
+    format: 'A4',
+    border: {
+        top: '0',
+        right: '10px',
+        bottom: '0',
+        left: '10px'
+    }
+}
+
+const content = template.replace("$$bodyRow$$", '<h1>Hola Mundo :D</h1>');
+pdf.create(content, options).toFile('./pdfs/result.pdf', (e) => {
+    console.log(e)
+})
