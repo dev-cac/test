665983a040a016dabeec135ef30d7bd6f35b6678
luisfosg <luisfosgdev@gmail.com>
2023-10-12 23:15:58 -0500

    feat: imagenes y carga

--- components/lib/src/routes/routes.dart
+++ components/lib/src/routes/routes.dart
@@ -3,11 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:components/src/pages/home_page.dart';
 import 'package:components/src/pages/alert_page.dart';
 import 'package:components/src/pages/avatar_page.dart';
+import 'package:components/src/pages/card_page.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes() {
   return <String, WidgetBuilder>{
     '/': (BuildContext context) => const HomePage(),
     'alert': (BuildContext context) => const AlertPage(),
     'avatar': (BuildContext context) => const AvatarPage(),
+    'card': (BuildContext context) => const CardPage(),
   };
 }
