53c360fd2455686822fc545208f3e863431c728c
luisfosg <luisfosgdev@gmail.com>
2023-10-13 09:51:35 -0500

    feat: alertas en flutter

--- components/lib/src/pages/alert_page.dart
+++ components/lib/src/pages/alert_page.dart
@@ -9,12 +9,54 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Alert Page'),
       ),
+      body: Center(
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blue.shade300,
+            shape: const StadiumBorder()
+          ),
+          onPressed: () {
+            _showAlert(context);
+          },
+          child: const Text('Mostrar Alerta'),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.ac_unit_rounded),
+        child: const Icon(Icons.ac_unit_rounded),
         onPressed: () {
           Navigator.pop(context);
         },
       ),
+    );
+  }
+
+  void _showAlert(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+          title: const Text('Titulo'),
+          content: const Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text('Este es el contenido de la caja de la alerta'),
+              FlutterLogo(size: 100)
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancelar'),
+              onPressed: () => Navigator.of(context).pop()
+            ),
+            TextButton(
+              child: const Text('Ok'),
+              onPressed: () => Navigator.of(context).pop()
+            ),
+          ],
+        );
+      }
     );
   }
 }
