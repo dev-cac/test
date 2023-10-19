ad3188baabb73eeabbbd7db9971c841a862e0ce8
luisfosg <luisfosgdev@gmail.com>
2023-10-19 16:57:12 -0500

    feat: instalando http

--- components/lib/main.dart
+++ components/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:components/src/providers/movies_provider.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -7,40 +8,50 @@ import 'package:provider/provider.dart';
 
 import 'package:components/src/services/socket_service.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(const AppState());
+
+class AppState extends StatelessWidget {
+  const AppState({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => MoviesProvider(), lazy: false),
+        ChangeNotifierProvider(create: (_) => SocketService())
+      ],
+      child: const MyApp(),
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => SocketService())
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
       ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        localizationsDelegates: const [
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-          GlobalCupertinoLocalizations.delegate,
-        ],
-        supportedLocales: const [
-          Locale('en', 'US'),
-          Locale('es', 'ES'),
-        ],
-        title: 'Componentes App',
-        initialRoute: '/',
-        routes: getApplicationRoutes(),
-        onGenerateRoute: (RouteSettings settings) {
-          return MaterialPageRoute(builder: (BuildContext context) => const AlertPage());
-        },
-        theme: ThemeData.light().copyWith(
-          appBarTheme: const AppBarTheme(
-            color: Colors.blueGrey
-          )
+      supportedLocales: const [
+        Locale('en', 'US'),
+        Locale('es', 'ES'),
+      ],
+      title: 'Componentes App',
+      initialRoute: '/',
+      routes: getApplicationRoutes(),
+      onGenerateRoute: (RouteSettings settings) {
+        return MaterialPageRoute(builder: (BuildContext context) => const AlertPage());
+      },
+      theme: ThemeData.light().copyWith(
+        appBarTheme: const AppBarTheme(
+          color: Colors.blueGrey
         )
-      ),
+      )
     );
   }
 }
