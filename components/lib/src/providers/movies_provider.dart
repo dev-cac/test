a24d3937b1776c6d1e731c2e3bb6105c4833d53f
luisfosg <luisfosgdev@gmail.com>
2023-10-19 23:02:57 -0500

    feat: creando modelos de los endpoints

--- components/lib/src/providers/movies_provider.dart
+++ components/lib/src/providers/movies_provider.dart
@@ -1,14 +1,14 @@
+import 'package:components/src/models/now_playing_response.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 
 class MoviesProvider extends ChangeNotifier {
-  String _baseUrl = 'api.themoviedb.org';
+  final String _baseUrl = 'api.themoviedb.org';
   String _apiKey = '';
-  String _language = 'es-ES';
+  final String _language = 'es-ES';
 
   MoviesProvider() {
-    print('MoviesProvider inicializado');
     getOnDisplayMovies();
   }
 
@@ -24,7 +24,7 @@ class MoviesProvider extends ChangeNotifier {
     var response = await http.get(url);
     if (response.statusCode != 200) return print('Error: get getOnDisplayMovies');
 
-    print('Response status: ${response.statusCode}');
-    print('Response body: ${response.body}');
+    final res = NowPlayingResponse.fromRawJson(response.body);
+    print(res.results[0].title);
   }
 }
