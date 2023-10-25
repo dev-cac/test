1b98c582a0b779f041af55f587b94ed12cde0b44
luisfosg <luisfosgdev@gmail.com>
2023-10-25 08:56:27 -0500

    feat: diseño con scroll

--- components/lib/src/pages/designs/scroll_design.dart
+++ components/lib/src/pages/designs/scroll_design.dart
@@ -5,13 +5,45 @@ class ScrollDesign extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Stack(
-        children: [
-          Background(),
-          MainContent()
-        ],
+    const gradientBg = BoxDecoration(
+      gradient: LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        stops: [0.5, 0.6],
+        colors: [
+          Color(0xff5EE8C5),
+          Color(0xff30BAD6)
+        ]
+      )
+    );
+
+    return Scaffold(
+      backgroundColor: const Color(0xff30BAD6),
+      body: Container(
+        decoration: gradientBg,
+        child: PageView(
+          physics: const BouncingScrollPhysics(),
+          scrollDirection: Axis.vertical,
+          children: const [
+            Page1(),
+            Page2()
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class Page1 extends StatelessWidget {
+  const Page1({ super.key });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Stack(
+      children: [
+        Background(),
+        MainContent()
+      ],
     );
   }
 }
@@ -54,6 +86,29 @@ class Background extends StatelessWidget {
       height: double.infinity,
       alignment: Alignment.topCenter,
       child: const Image(image: AssetImage('assets/images/scroll-1.png'))
+    );
+  }
+}
+
+/* ========================================== */
+
+class Page2 extends StatelessWidget {
+  const Page2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color(0xff30BAD6),
+      child: Center(
+        child: TextButton(
+          style: TextButton.styleFrom(backgroundColor: const Color(0xff0098FA), shape: const StadiumBorder()),
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: Text('Bienvenido', style: TextStyle(color: Colors.white, fontSize: 30)),
+          ),
+          onPressed: () {},
+        )
+        ),
     );
   }
 }
