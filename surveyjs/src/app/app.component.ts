59f936c07375d32d7938bed99d97bb7520112769
luisfosg <luisfosgdev@gmail.com>
2023-10-26 14:34:14 -0500

    feat: basic surveyjs

--- surveyjs/src/app/app.component.ts
+++ surveyjs/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as Survey from 'survey-angular';
+
+import surveyJson from './survey.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'surveyjs';
+export class AppComponent implements OnInit {
+  survey: Survey.Model | undefined;
+  jsonSurvey: any;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    /* this._anyService.getSurvey().subscribe((res) => {
+      this.jsonSurvey = res.data;
+    }); */
+
+    this.jsonSurvey = surveyJson;
+
+    this.survey = new Survey.Model(this.jsonSurvey);
+    this.survey.onComplete.add(this.onSurveyComplete);
+
+    Survey.SurveyNG.render("surveyElement", { model: this.survey });
+  }
+
+  onSurveyComplete = (result: { data: any; }) => {
+    console.log(result.data);
+  }
 }
